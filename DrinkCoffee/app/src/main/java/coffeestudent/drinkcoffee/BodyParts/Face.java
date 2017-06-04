3225eb3c29706fa2d40a37ee54b2aef710ebbbf7
Steven Hieu Huynh <stevenhh@uci.edu>
2017-06-03 18:58:48 -0700

    added face

---
+++ DrinkCoffee/app/src/main/java/coffeestudent/drinkcoffee/BodyParts/Face.java
@@ -0,0 +1,14 @@
+package coffeestudent.drinkcoffee.BodyParts;
+
+import android.graphics.Bitmap;
+
+/**
+ * Created by huynh on 6/3/2017.
+ */
+
+public class Face {
+    Bitmap bitmap;
+    public Face(){
+
+    }
+}
